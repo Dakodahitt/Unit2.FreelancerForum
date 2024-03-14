cd4e3787588714b4bbd43abd3cbeba8c5daad268
Dakoda hitt <dakoda@MacBook-Air.local>
2024-03-14 12:52:34 -0600

    added freelancers

--- index.js
+++ index.js
@@ -1,8 +1,67 @@
 // Possible  names and occupations
+const names = [
+  "Sophia",
+  "Liam",
+  "Emma",
+  "Noah",
+  "Olivia",
+  "William",
+  "Ava",
+  "James",
+  "Isabella",
+  "Oliver",
+  "Mia",
+  "Benjamin",
+  "Charlotte",
+  "Elijah",
+  "Amelia",
+  "Lucas",
+  "Harper",
+  "Mason",
+  "Evelyn",
+  "Ethan",
+];
+const occupations = [
+    "writer",
+    "teacher",
+    "programmer",
+    "designer",
+    "consultant",
+    "engineer",
+    "artist",
+    "doctor",
+    "nurse",
+    "architect",
+    "chef",
+    "musician",
+    "photographer",
+    "marketing specialist",
+    "lawyer",
+    "scientist",
+    "accountant",
+    "entrepreneur",
+    "salesperson",
+    "translator",
+  ];
+// arry of freelancers
+let freelancer = [
+    { name:"James", price: 30, occupation:"writer"},
+    { name:"Ava", price: 70, occupation: "designer"}
+];
+//
 
 
-function calculateAveragePrice() {
-    const totalPrices = freelancers.reduce((acc, freelancer) => acc + freelancer.price, 0);
-    const averagePrice = totalPrices / freelancers.length;
-    return averagePrice.toFixed(2);
-}
+
+
+
+
+
+
+// function calculateAveragePrice() {
+//   const totalPrices = freelancers.reduce(
+//     (acc, freelancer) => acc + freelancer.price,
+//     0
+//   );
+//   const averagePrice = totalPrices / freelancers.length;
+//   return averagePrice.toFixed(2);
+// }
